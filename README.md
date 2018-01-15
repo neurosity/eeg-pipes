6a784a6d1f13c953d4404dd3dc6037ac4a2c598d
jdpigeon <morrisondano@gmail.com>
2018-01-15 17:42:38 -0500

    testing changes to notch filter

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ The following are some libraries that provide exactly that:
 * [OpenBCI Cyton/Ganglion (Node)](https://github.com/alexcastillo/openbci-rx)
 * [Muse (Browser)](https://github.com/urish/muse-js)
 
-Pipes can be added to an EEG observable of EEG data samples with the 
+Pipes can be added to an EEG observable of EEG data samples with the
 following data structure:
 
 ``` js
@@ -57,7 +57,7 @@ eeg$.pipe(
 
 * bufferFFT({ bins, window, sampleRate })
 
-Using frequency pipes requires a buffer of samples. 
+Using frequency pipes requires a buffer of samples.
 This can be accomplished by using bufferFFT first OR bufferCount/bufferTime.
 
 * alphaRange()
@@ -70,13 +70,13 @@ This can be accomplished by using bufferFFT first OR bufferCount/bufferTime.
 * toMicrovolts({ log })
 
 #### Utility
-* bufferCount()
-* bufferTime()
+* [bufferCount()](http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-bufferCount)
+* [bufferTime()](http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-bufferTime)
 * groupByChannel()
 
 ### Coming soon
 
-#### Filtering 
+#### Filtering
 * bandPassFilter()
 * vertScaleFilter()
 * vertAgoFilter()
--- src/pipes/filtering/notchFilter.js
+++ src/pipes/filtering/notchFilter.js
@@ -1,43 +1,64 @@
+import { CalcCascades, IirFilter } from "fili";
+import { map } from "rxjs/operators";
 
-import { CalcCascades, IirFilter } from 'fili';
-import { map } from 'rxjs/operators';
+import { createPipe } from "../../utils/createPipe";
 
-import { createPipe } from '../../utils/createPipe';
-
-import {
-    SAMPLE_RATE as defaultSampleRate
-} from '../../constants';
+import { SAMPLE_RATE as defaultSampleRate } from "../../constants";
 
 /**
  * @method notchFilter
  * Applies notch filter to FFT buffer
- * 
+ *
  * @param {Object} options
  * @returns {Observable}
  */
 export const notchFilter = ({
-    order = 2,
-    characteristic = 'butterworth',
-    cutoffFrequency = 50,
-    sampleRate = defaultSampleRate,
-    Fs = sampleRate,
-    Fc = cutoffFrequency,
-    gain = 0, 
-    preGain = false
-} = {}) =>
-    source => createPipe(
-        source,
-        map(channelGroupBuffer => {
+  order = 2,
+  characteristic = "butterworth",
+  cutoffFrequency = 50,
+  sampleRate = defaultSampleRate,
+  nbChannels = 4,
+  gain = 0,
+  preGain = false,
+  BW = 0.1
+} = {}) => source$ => {
+  const options = {
+    order,
+    characteristic,
+    sampleRate,
+    cutoffFrequency,
+    gain,
+    preGain,
+    BW
+  };
+  const notchFilterGroup = new Array(nbChannels).fill(notchIIR(options));
+  console.log(
+    "created options object: ",
+    options,
+    " and filterGroup: ",
+    notchFilterGroup
+  );
 
-            const notch = channelGroup => {
-                const options = { order, characteristic, Fs, Fc, gain, preGain };
-                const calc = new CalcCascades();
-                const coeffs = calc.bandstop(options);
-                const filter = new IirFilter(coeffs);
-                
-                return filter.multiStep(channelGroup);
-            };
+  createPipe(
+    source$,
+    map(channelGroupBuffer => {
+      console.log("mapping through channelGroupBuffer: ", channelGroupBuffer);
+      return channelGroupBuffer.map((channelGroup, index) => {
+        console.log("mapping through channelGroups: ", channelGroup);
+        notchFilterGroup[index];
+      });
+    })
+  );
+};
 
-            return channelGroupBuffer.map(notch);
-        })
-    );
+// -----------------------------------------------------------------------------
+// Helper Functions
+
+// Constructs an IIR bandstop filter from given options and returns a multistep filter function
+const notchIIR = options => {
+  const calc = new CalcCascades();
+  const coeffs = calc.bandstop(options);
+  const filter = new IirFilter(coeffs);
+
+  return filter.multiStep(channelGroup);
+};
