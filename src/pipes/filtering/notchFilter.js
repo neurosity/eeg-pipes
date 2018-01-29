51db029ed5b06dc5a4d72a256885add36f3f92db
jdpigeon <morrisondano@gmail.com>
2018-01-29 17:10:24 -0500

    updated notchFilter to use simple filter persistence strategy

--- src/pipes/filtering/notchFilter.js
+++ src/pipes/filtering/notchFilter.js
@@ -1,20 +1,16 @@
 import { CalcCascades, IirFilter } from "fili";
-import { scan, map } from "rxjs/operators";
-
+import { map } from "rxjs/operators";
 import { createPipe } from "../../utils/createPipe";
 import {
-  CHANNELS as defaultNbChannels,
   SAMPLE_RATE as defaultSampleRate,
   ORDER as defaultOrder,
-  CHARACTERISTIC as defaultCharacteristic,
-  GAIN as defaultGain,
-  PREGAIN as defaultPreGain
+  CHARACTERISTIC as defaultCharacteristic
 } from "../../constants";
 
 /**
  * @method notchFilter
- * Applies notch filter to a buffer of EEG data
- *
+ * Applies notch filter to EEG Data. Can be applied to Samples or Chunks. Must provide nbChannels. cutOffFrequency will default to 60hz.
+ * @example { nbChannels = 4, samplingRate = 256, cutOffFrequency = 60 }
  * @param {Object} options
  * @returns {Observable}
  */
@@ -25,36 +21,51 @@ const createNotchIIR = options => {
   return new IirFilter(coeffs);
 };
 
-export const notchFilter = (
-  {
-    order = defaultOrder,
-    characteristic = defaultCharacteristic,
-    cutoffFrequency = 60,
-    sampleRate = defaultSampleRate,
-    Fs = sampleRate,
-    Fc = cutoffFrequency,
-    gain = defaultGain,
-    preGain = defaultPreGain,
-    BW = 0.1,
-    nbChannels = defaultNbChannels
-  } = {}
-) => source =>
-  createPipe(
+export const notchFilter = ({
+  nbChannels,
+  order = defaultOrder,
+  characteristic = defaultCharacteristic,
+  cutoffFrequency = 60,
+  samplingRate = defaultSampleRate,
+  Fs = samplingRate,
+  Fc = cutoffFrequency,
+  BW = 0.1
+} = {}) => source => {
+  if (!nbChannels) {
+    throw new Error(
+      "Please supply nbChannels parameter to notchFilter operator"
+    );
+  }
+  const options = {
+    order,
+    characteristic,
+    Fs,
+    Fc,
+    BW
+  };
+  const notchArray = new Array(nbChannels)
+    .fill(0)
+    .map(() => createNotchIIR(options));
+  return createPipe(
     source,
-    scan(
-      (acc, curr) => [
-        curr.map((channel, index) => acc[1][index].multiStep(channel)),
-        acc[1]
-      ],
-      [
-        new Array(nbChannels).fill(0),
-        new Array(nbChannels)
-          .fill(0)
-          .map(x =>
-            createNotchIIR({ order, characteristic, Fs, Fc, gain, preGain, BW })
+    map(eegObject => {
+      // Should run multiStep function if inc. data is a Chunk (2D data array)
+      if (Array.isArray(eegObject.data[0])) {
+        return {
+          ...eegObject,
+          data: eegObject.data.map((channel, index) =>
+            notchArray[index].multiStep(channel)
           )
-      ]
-    ),
-    // Pluck just the data array to emit
-    map(dataAndFilter => dataAndFilter[0])
+        };
+      }
+
+      // Should run singleStep if inc. data is a Sample (1D array)
+      return {
+        ...eegObject,
+        data: eegObject.data.map((channel, index) =>
+          notchArray[index].singleStep(channel)
+        )
+      };
+    })
   );
+};
