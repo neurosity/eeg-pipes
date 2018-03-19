3efbc006ef7dd44420c8aaae35d00186b4ca1f02
Alex Castillo <alex@castillo.io>
2018-03-18 23:20:25 -0600

    webpack 4 upgrade: added externals and code splitting support

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
+const webpackRxjsExternals = require('webpack-rxjs-externals');
+const nodeExternals = require('webpack-node-externals');
 
 const library = 'EEGPipes';
 const libraryFileName = 'eeg-pipes';
 
 const config = {
-    entry: './src/index.js',
+    entry: {
+        main: './src/index.js'
+    },
     module: {
         rules: [
             {
@@ -13,9 +17,25 @@ const config = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.csv$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'csv-loader',
+                    options: {
+                        dynamicTyping: true,
+                        header: false,
+                        skipEmptyLines: true
+                    }
+                }
             }
         ]
     },
+    externals: [
+        nodeExternals({}),
+        webpackRxjsExternals()
+    ],
     devtool: 'eval',
     devServer: {
         compress: true,
@@ -28,8 +48,10 @@ const exportLibraryTarget = libraryTarget =>
         output: {
             path: path.resolve(__dirname, 'dist'),
             filename: `${libraryFileName}.${libraryTarget}.js`,
+            chunkFilename: `${libraryFileName}.[name].${libraryTarget}.js`,
             library,
-            libraryTarget
+            libraryTarget,
+            globalObject: 'this'
         }
     });
 
