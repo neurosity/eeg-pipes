51a0d4d8fa08a5d628e7b9dfb10e6343d7b8f3d3
jdpigeon <morrisondano@gmail.com>
2018-04-13 15:25:33 -0400

    Removed webpack externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,39 @@
 const path = require("path");
-const webpackRxjsExternals = require("webpack-rxjs-externals");
-const nodeExternals = require("webpack-node-externals");
 
 const library = "EEGPipes";
 const libraryFileName = "eeg-pipes";
 
 const config = {
-  entry: {
-    main: "./src/index.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  },
-  externals: [nodeExternals({}), webpackRxjsExternals()],
-  devtool: "eval",
-  devServer: {
-    compress: true,
-    port: 9000
-  }
+    entry: {
+        main: "./src/index.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    devtool: "eval",
+    devServer: {
+        compress: true,
+        port: 9000
+    }
 };
 
-const exportLibraryTarget = libraryTarget =>
-  Object.assign({}, config, {
+const exportLibraryTarget = libraryTarget => Object.assign({}, config, {
     output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: `${libraryFileName}.${libraryTarget}.js`,
-      chunkFilename: `${libraryFileName}.[name].${libraryTarget}.js`,
-      library,
-      libraryTarget,
-      globalObject: "this"
+        path: path.resolve(__dirname, "dist"),
+        filename: `${libraryFileName}.${libraryTarget}.js`,
+        chunkFilename: `${libraryFileName}.[name].${libraryTarget}.js`,
+        library,
+        libraryTarget,
+        globalObject: "this"
     }
-  });
+});
 
 module.exports = [exportLibraryTarget("umd"), exportLibraryTarget("var")];
