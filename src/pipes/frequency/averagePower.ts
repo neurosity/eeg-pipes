6b8f419624205bc791aef589908a80339c1e9698
Yannick <yannick.adam@gmail.com>
2020-10-30 11:53:06 +0100

    AbsolutePower + mapper for powerByBand

--- src/pipes/frequency/averagePower.ts
+++ src/pipes/frequency/averagePower.ts
@@ -5,7 +5,7 @@ import { average } from "../../utils/stats";
 import { IPSD } from "../../types/psd";
 
 /**
- * Takes a stream of PSDs and returns a sream of arrays, containing the average power in each channel
+ * Takes a stream of PSDs and returns a stream of arrays, containing the average power in each channel
  * @method averagePower
  * @example eeg$.pipe(epoch({ duration: 256, interval: 100, samplingRate: 256 }), fft({ bins: 256 }), sliceFFT(10, 20), averagePower())
  * @returns {Observable} band powers array
